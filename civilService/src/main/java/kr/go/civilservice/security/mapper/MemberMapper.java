653c093b4e6d269dd31e87736f9a06a34e53b3a0
LWH <lwhprt@naver.com>
2025-01-01 22:23:55 +0900

    Feat : 파라미터 바인딩 문제 해결 및 오타 수정 및 포트수정
    파라미터 바인딩시 명시적으로 하지 않아서발생한 오류 해결, response body부분 json으로 하기 위해 의존성 추가 및 스프링 버전 up

--- civilService/src/main/java/kr/go/civilservice/security/mapper/MemberMapper.java
+++ civilService/src/main/java/kr/go/civilservice/security/mapper/MemberMapper.java
@@ -1,14 +1,23 @@
 package kr.go.civilservice.security.mapper;
 
 import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import kr.go.civilservice.security.model.MemberVO;
 
 public interface MemberMapper {
-    MemberVO getMemberById(String memberId);
-    List<String> getMemberAuthorities(String memberId);
-    int insertMember(MemberVO member);
-    int insertAuthority(String memberId, String authority);
-    int updateMember(MemberVO member);
-    int updateLoginFailCount(String memberId, int count);
-    int updateLastLoginDate(String memberId);
+	MemberVO getMemberById(String memberId);
+
+	List<String> getMemberAuthorities(String memberId);
+
+	int insertMember(MemberVO member);
+
+	int insertAuthority(@Param("memberId") String memberId, @Param("authority") String authority);
+
+	int updateMember(MemberVO member);
+
+	int updateLoginFailCount(String memberId, int count);
+
+	int updateLastLoginDate(String memberId);
 }
